bcb47d0f307f264b76500a7e797e08adc2b331d8
alumno <alumno@macbook-de-admin-2.local>
2017-01-31 19:36:31 -0500

    Initial Commit

---
+++ Navegacion en iOs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navegacion en iOs
+//
+//  Created by alumno on 31/01/17.
+//  Copyright © 2017 alg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
